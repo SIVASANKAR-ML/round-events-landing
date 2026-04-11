db97edfee397551a888f7dfd2380a1f89bc6c1ad
SIVASANKAR-ML <msivasankar241@gmail.com>
2026-04-11 23:37:37 +0530

    doctype created

---
+++ round_events/round_events/doctype/re_job_application/test_re_job_application.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, siva and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestREJobApplication(FrappeTestCase):
+	pass
